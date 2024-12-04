789e17d57041ebc5d67ca1176d5919aa86450840
Darragh24 <conwaydarragh03@gmail.com>
2024-12-04 12:30:52 +0000

    ➕ Fixed the imports/dependencies so the Animation can work.

--- app/src/main/java/com/example/darragh_richy_ca3/MainActivity.kt
+++ app/src/main/java/com/example/darragh_richy_ca3/MainActivity.kt
@@ -3,16 +3,7 @@ package com.example.darragh_richy_ca3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.darragh_richy_ca3.components.ExpandableCardListScreen
-import com.example.darragh_richy_ca3.ui.theme.Darragh_Richy_CA3Them
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/darragh_richy_ca3/components/Animations.kt
+++ app/src/main/java/com/example/darragh_richy_ca3/components/Animations.kt
@@ -1,25 +1,29 @@
 package com.example.darragh_richy_ca3
-import androidx.compose.animation.core.*
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberInfiniteTransition
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun FallingLeafAnimation() {
-    val infiniteTransition = rememberInfiniteTransition()
+    val infiniteTransition = rememberInfiniteTransition(label = "")
     val offsetY by infiniteTransition.animateFloat(
         initialValue = -200f,
         targetValue = 800f,
         animationSpec = infiniteRepeatable(
             animation = tween(durationMillis = 5000, easing = LinearEasing),
             repeatMode = RepeatMode.Restart
-        )
+        ), label = ""
     )
 
     Image(
